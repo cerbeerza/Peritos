6190ba8b777b06dfdcf44e51b584277c4820ff8d
cerbeerza <cerbeerza@gmail.com>
2018-02-20 11:55:48 -0300

    2 VIEWS GENERIC AND NO

--- apps/prueba/urls.py
+++ apps/prueba/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import url, include
 from django.contrib.auth.decorators import login_required
 
-from apps.prueba.views import PruebaList
+from apps.prueba.views import PruebaList, pruebaListFn
 
 urlpatterns = [
 
     url(r'^listar$', login_required(PruebaList.as_view()), name='prueba_listar'),
+    #url(r'^listar2$', pruebaListFn, name='prueba_listar'),
 ]
 
--- apps/prueba/views.py
+++ apps/prueba/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render
 from django.views.generic import ListView
-
-
 from apps.prueba.models import Prueba
 
 
 
 class PruebaList(ListView):
+
     model = Prueba
+    queryset =  Prueba.objects.filter(periodo='2016')
     template_name = 'prueba/prueba_list.html'
+
+
+def pruebaListFn(request):
+    resultado = Prueba.objects.filter(periodo='2015')
+
+    return render(request, 'templates/prueba/prueba_list.html',{'objeto': resultado} )
+
