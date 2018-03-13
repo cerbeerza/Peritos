2e04732d3060180b4854159121c4b9498cbf5883
cerbeerza <cerbeerza@gmail.com>
2018-03-13 16:55:26 -0300

    PEGA: Modificacion NOTAS

--- apps/mensura/views.py
+++ apps/mensura/views.py
@@ -7,6 +7,10 @@ import requests, json
 from apps.mensura.models import MensuraGeneral
 
 
+class Struct(object):
+    def __init__(self, **entries):
+        self.__dict__.update(entries)
+
 class MensuraGeneralList(ListView):
     model = MensuraGeneral
     template_name = 'mensura/mensura_general_list.html'
@@ -15,7 +19,21 @@ def mensuraGeneralFn(request):
 
     if request.method == "GET":
 
-        return render(request, 'templates/mensura/mensura_general_list.html', {'objeto' : None})
+        cabeceras = {
+            "Content-Type": "application/json"
+        }
+
+        datos = '{ "pass": "sngmq21.,+"}'
+
+
+        periodos = requests.post("http://172.16.61.214:8081/NotasPeritosREST/service/NotasPeritos/getPeriodos",
+                                 data=datos, headers=cabeceras)
+
+        listPeriodos = periodos.json()
+
+
+
+        return render(request, 'templates/mensura/mensura_general_list.html', {'listaPeriodos' : listPeriodos})
 
     elif request.method == "POST":
 
@@ -31,11 +49,24 @@ def mensuraGeneralFn(request):
         datoJson = response.json()
         notaPromedio = datoJson['notaPromedio']
         movPer = datoJson['movPerito']
+        empresa = datoJson['empresa']
+        nombrePer = datoJson['nomper']
+        situacion = None
+
+        if float(notaPromedio) < 4:
+            situacion = "Reprobado"
+        else:
+            situacion = "Aprobado"
 
 
-        objeto = {"notaPromedio": notaPromedio, "movPer": movPer, "periodo": periodo}
 
-        return render(request, 'templates/mensura/mensura_general_list.html', {'objeto' : objeto})
+
+        objeto = {"notaPromedio": notaPromedio, "movPer": movPer, "periodo": periodo, "empresa": empresa, "nombrePer" : nombrePer, "situacion" : situacion}
+
+        objeto2 = Struct(**objeto)
+
+
+        return render(request, 'templates/mensura/mensura_general_list.html', {'objeto2' : objeto2})
 
 
 
