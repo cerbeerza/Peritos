4248125de5fa89d6337b4b1341e27d642549ed5f
cerbeerza <cerbeerza@gmail.com>
2018-03-16 15:48:09 -0300

    CAMBIO DE BASE

--- Peritos/settings.py
+++ Peritos/settings.py
@@ -88,10 +88,15 @@ WSGI_APPLICATION = 'Peritos.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'peritos',
+        'USER': 'root',
+        'PASSWORD': 'Peritos0104',
+        'HOST': '172.16.60.35',
+        'PORT': '3306',
     }
 }
 
