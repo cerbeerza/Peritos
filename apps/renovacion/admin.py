1e4b206627b25a229e316a3511deb37dca399cc4
cerbeerza <cerbeerza@gmail.com>
2018-05-22 16:56:31 -0400

    cambios rod pega

--- apps/renovacion/admin.py
+++ apps/renovacion/admin.py
@@ -1,4 +1,19 @@
 from django.contrib import admin
 from apps.renovacion.models import Renovacion
 
-admin.site.register(Renovacion)
+
+
+class RenovacionAdmin(admin.ModelAdmin):
+    list_display = ('id_user', 'get_nombre', 'fecha_creacion', 'periodo')
+    list_filter = ['periodo']
+    readonly_fields = ['archivo_ci', 'archivo_ant', 'archivo_tit']
+
+    def get_nombre(self, instance):
+        return instance.id_user.profile.nombres
+    get_nombre.short_description = "Nombres"
+
+
+
+
+
+admin.site.register(Renovacion, RenovacionAdmin)
