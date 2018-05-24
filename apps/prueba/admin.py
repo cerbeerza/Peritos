8bfad9b565313c4bc30a82141102dacce7eb992c
cerbeerza <cerbeerza@gmail.com>
2018-05-24 17:06:42 -0400

    cambios rod pega

--- apps/prueba/admin.py
+++ apps/prueba/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from apps.prueba.models import Prueba
+from import_export.admin import ImportExportModelAdmin
 
-class PruebaAdmin(admin.ModelAdmin):
+class PruebaAdmin(ImportExportModelAdmin):
     list_display = ('nombre', 'rut', 'nota', 'situacion', 'periodo')
     list_filter = ['periodo']
+
 
 admin.site.register(Prueba, PruebaAdmin)
